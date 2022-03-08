76c69d697a5804ad495d0a8ecb1849e24deee6c4
Calum Knott <calumk@Calums-MacBook-Pro.local>
2022-03-08 14:37:11 +0000

    removed github actions

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 
+const app = createApp(App)
 
-createApp(App)
-  .mount('#app')
-  .$nextTick(window.removeLoading)
+app.use(ElementPlus)
+app.mount('#app')
+app.$nextTick(window.removeLoading)
 
 // console.log('fs', window.fs)
 // console.log('ipcRenderer', window.ipcRenderer)
